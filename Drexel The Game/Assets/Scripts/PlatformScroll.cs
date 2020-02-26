51a984f02cfce5f89f1371db4cd8b4e672470680
Kid Sizzle <cd3258@drexel.edu>
2020-02-26 14:40:11 -0500

    Base project

---
+++ Drexel The Game/Assets/Scripts/PlatformScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScroll : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // every frame: move the platform left multiplied by the frame speed and the speed set for the platform  
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if the platform collides with an object with the tag "ObjectBoundary"
+        if (collision.gameObject.CompareTag("ObjectBoundary"))
+        {
+            // destroy Platform
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Drexel The Game/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public Rigidbody2D playerRigi;
+    public float jumpForce;
+    public int numOfJumps = 2;
+    private int jumpCounter = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the user presses "w", "spacebar", or "upArrow" while the jumpCounter is greater than 0
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown("space") || Input.GetKeyDown("up")) && jumpCounter > 0)
+        {
+            // set player's rigidbody upwards velocity (Vector for 2D object) equal to the public float jumpForce. Then subract jumps by 1
+            // a.k.a. allow it to jump and subtract a jump from it's jump counter (double jump)
+            playerRigi.velocity = new Vector2(0, jumpForce);
+            jumpCounter = jumpCounter - 1;
+            
+        }   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if the Player collides with a platform with the tag "platform"
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            // reset jump counter to 2 (numOfJumps)
+            jumpCounter = numOfJumps;
+        }
+        // if the Player collides with a hazard (ex. spikes, traps, etc.)
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            // destroy Player
+            Destroy(gameObject);
+        }
+
+    }
+}
