173b7c07375530c45ee19ca85a71b8018ef4571f
Kid Sizzle <cd3258@drexel.edu>
2020-03-13 22:40:52 -0400

    Updated up to CI102 Demo

---
+++ Drexel The Game/Assets/Scripts/Restart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if enter is pressed
+        if (Input.GetKeyDown("return"))
+        {
+            // load next scene
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Drexel The Game/Assets/Scripts/Tutorial Scripts/SpriteTextScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteTextScript : MonoBehaviour
+{
+    public int Active = 0;
+    public Text SpikeText;
+    public Text FText;
+    public Text CoinText;
+    public Text AText;
+    public Text EndText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if Active equals 0
+        if (Active == 0)
+        {
+            // set all texts to invisible
+            SpikeText.GetComponent<Text>().enabled = false;
+            FText.GetComponent<Text>().enabled = false;
+            CoinText.GetComponent<Text>().enabled = false;
+            AText.GetComponent<Text>().enabled = false;
+            EndText.GetComponent<Text>().enabled = false;
+        }
+
+        // if Active is 1
+        if (Active == 1)
+        {
+            // set SpikeText to visible
+            SpikeText.GetComponent<Text>().enabled = true;
+        }
+
+        // if Active is 2
+        if (Active == 2)
+        {
+            // set Ftext to visible
+            FText.GetComponent<Text>().enabled = true;
+        }
+
+        // if Active is 3
+        if (Active == 3)
+        {
+            // set CoinText to visible
+            CoinText.GetComponent<Text>().enabled = true;
+        }
+
+        // if Active is 4
+        if (Active == 4)
+        {   // set Atext to visible
+            AText.GetComponent<Text>().enabled = true;
+        }
+
+        // if Active is 5
+        if (Active == 5)
+        {
+            // set EndText to visible
+            EndText.GetComponent<Text>().enabled = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if Player collides with Activate1
+        if (collision.gameObject.CompareTag("Activate1"))
+        {
+            // set Active to 1
+            Active = 1;
+        }
+
+        // if Player collides with Activate2
+        if (collision.gameObject.CompareTag("Activate2"))
+        {
+            // set Active to 2
+            Active = 2;
+        }
+
+        // if Player collides with Activate3
+        if (collision.gameObject.CompareTag("Activate3"))
+        {
+            // set Active to 3
+            Active = 3;
+        }
+
+        // if Player collides with Activate4
+        if (collision.gameObject.CompareTag("Activate4"))
+        {
+            // set Active to 4
+            Active = 4;
+        }
+
+        // if Player collides with Activate5
+        if (collision.gameObject.CompareTag("Activate5"))
+        {
+            // set Active to 5
+            Active = 5;
+        }
+
+        // if Player collides with Deactivate
+        if (collision.gameObject.CompareTag("Deactivate"))
+        {
+            // set Active to 0
+            Active = 0;
+        }
+    }
+    
+}
